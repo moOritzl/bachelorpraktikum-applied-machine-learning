2fc56da2aab5fa7210c9856f0023c8327135cf09
Moritz Lenhard <123739316+flatterfan@users.noreply.github.com>
2024-11-12 18:29:23 +0100

    initial idea for the model, still needs major rework, there is no improvement during the epochs

--- Week_05/data_handler.py
+++ Week_05/data_handler.py
@@ -11,10 +11,11 @@ def split_data(X, y, frac, random_state=69):
     :param y: Dependent variables.
     :param frac: Fraction of data used for training.
     :param random_state: Random seed for splitting the data.
-    :return: X_train, X_test, y_train, y_test.
+    :return: X_train, X_test, y_train, y_test as Numpy arrays.
     """
-    Xtrain = X.sample(frac=frac, axis=0, random_state=random_state)
-    Xtest = X.drop(Xtrain.index)
-    ytrain = y.loc[Xtrain.index]
-    ytest = y.loc[Xtest.index]
-    return Xtrain.values, Xtest.values, ytrain.values.flatten(), ytest.values.flatten()
+    X_train = X.sample(frac=frac, axis=0, random_state=random_state)
+    X_test = X.drop(X_train.index)
+    y_train = y.loc[X_train.index]
+    y_test = y.loc[X_test.index]
+
+    return X_train.values, X_test.values, y_train.values.flatten(), y_test.values.flatten()
